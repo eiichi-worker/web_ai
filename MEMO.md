a9309d31813d537116b47c9f0839bff9117863fa
Eiichi Fujimoto <work.eiichi.co.jp@gmail.com>
2018-05-13 18:55:02 +0900

    MNISTサンプル追加

---
+++ MEMO.md
@@ -0,0 +1,12 @@
+template
+https://github.com/creativetimofficial/material-kit
+
+テンプレートエンジン「nunjucks」でサイトづくり  
+
+準備
+```
+npm i -g gulp
+npm i -g parcel-bundler
+npm i
+```
+
---
+++ gulpfile.js
@@ -0,0 +1,86 @@
+var gulp = require('gulp')
+var nunjucksRender = require('gulp-nunjucks-render')
+var runSequence = require('run-sequence')
+var del = require('del')
+var browserSync = require('browser-sync')
+var reload = browserSync.reload
+var ghPages = require('gulp-gh-pages')
+var exec = require('gulp-exec');
+
+gulp.task('default', function(callback) {
+  return runSequence(
+    'clean', [
+      'nunjucks',
+      'es6',
+      'static'
+    ],
+    callback
+  )
+})
+
+gulp.task('deploy', function(callback) {
+  return runSequence(
+    'clean', [
+      'nunjucks',
+      'es6',
+      'static'
+    ],
+    'github-page-push',
+    callback
+  )
+})
+
+gulp.task('clean', function(cb) {
+  return del(['dist/**/*'], cb)
+})
+
+gulp.task('nunjucks', function() {
+  return gulp.src([
+    'src/template/**/*.njk',
+    '!src/template/_layout/**/*.njk',
+    '!src/template/_parts/**/*.njk'
+  ]).pipe(nunjucksRender({
+    path: ['src/template/']
+  })).pipe(gulp.dest('dist'))
+})
+
+gulp.task('es6', function() {
+  var options = {
+    continueOnError: false, // default = false, true means don't emit error event
+    pipeStdout: false, // default = false, true means stdout is written to file.contents
+    src: ['src/es6/tfjs/mnist/index.es6'], // content passed to lodash.template()
+    dist: "dist/js/tfjs/mnist/" // content passed to lodash.template()
+  }
+  var reportOptions = {
+    err: true, // default = true, false means don't write err
+    stderr: true, // default = true, false means don't write stderr
+    stdout: true // default = true, false means don't write stdout
+  }
+
+  return gulp.src(options.src)
+    .pipe(exec('parcel build <%= file.path %> --no-minify -d <%= options.dist %>', options))
+    .pipe(exec.reporter(reportOptions))
+})
+
+gulp.task('static', function() {
+  return gulp.src('src/static/**/*').pipe(gulp.dest('dist/'))
+})
+
+gulp.task('server', function() {
+  browserSync({
+    notify: false,
+    server: {
+      baseDir: 'dist'
+    }
+  })
+
+  gulp.watch('src/static/**/*', ['static'])
+  gulp.watch('src/template/**/*', ['nunjucks'])
+  gulp.watch('src/es6/**/*', ['es6'])
+  gulp.watch('dist/**/*', reload)
+})
+
+gulp.task('github-page-push', function() {
+  return gulp.src('dist/**/*')
+    .pipe(ghPages())
+})
